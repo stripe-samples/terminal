74fc7c211329ea79b8fb0c68a0d1f732277de5a7
CJ Avilla <cjavilla@stripe.com>
2022-04-13 17:56:58 -0400

    Adding tests

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,15 +1,75 @@
 require_relative './spec_helper.rb'
 
-RSpec.describe "<your integration name> integration" do
+RSpec.describe "Terminal integration" do
   it "serves the index route" do
     # Get the index html page
     response = get("/")
     expect(response).not_to be_nil
   end
 
-  it "serves config with publishableKey" do
-    resp = get_json("/config")
-    expect(resp).to have_key("publishableKey")
-    expect(resp['publishableKey']).to start_with("pk_test")
+  it "lists the readers" do
+    ensure_simulated_readers
+    resp = get_json("/list-readers")
+    expect(resp).to have_key("readers")
+    expect(resp["readers"].length).to be > 0
+  end
+
+  it 'creates payment intents' do
+    amount_to_charge = 888
+    resp, status = post_json('/create-payment-intent', {
+      amount: amount_to_charge
+    })
+    expect(status).to eq(200)
+    expect(resp).to have_key('payment_intent_id')
+    payment_intent = Stripe::PaymentIntent.retrieve(resp['payment_intent_id'])
+    expect(payment_intent.amount).to eq(amount_to_charge)
+    expect(payment_intent.currency).to eq('usd')
+    expect(payment_intent.capture_method).to eq('manual')
+    expect(payment_intent.payment_method_types).to contain_exactly('card_present')
+  end
+
+  it 'retrieves payment intents' do
+    amount_to_charge = 888
+    payment_intent = Stripe::PaymentIntent.create(amount: amount_to_charge, currency: 'usd')
+
+    resp = get_json("/retrieve-payment-intent?payment_intent_id=#{payment_intent.id}")
+    expect(resp).to have_key("payment_intent")
+    expect(resp["payment_intent"]["id"]).to eq(payment_intent.id)
+  end
+
+  it 'processes payment' do
+    reader = Stripe::Terminal::Reader.list(limit: 1, device_type: 'simulated_wisepos_e').data.first
+    payment_intent = Stripe::PaymentIntent.create(
+      amount: 999,
+      currency: 'usd',
+      payment_method_types: ['card_present'],
+      capture_method: 'manual'
+    )
+    resp, status = post_json("/process-payment-intent", {
+      reader_id: reader.id,
+      payment_intent_id: payment_intent.id
+    })
+    expect(resp).to have_key("reader_state")
+    expect(resp["reader_state"]).to have_key("id")
+    expect(resp["reader_state"]["id"]).to eq(reader.id)
+    expect(resp["reader_state"]).to eq("")
+  end
+
+  it 'simulates payment' do
+    reader = Stripe::Terminal::Reader.list(limit: 1, device_type: 'simulated_wisepos_e').data.first
+    payment_intent = Stripe::PaymentIntent.create(
+      amount: 999,
+      currency: 'usd',
+      payment_method_types: ['card_present'],
+      capture_method: 'manual'
+    )
+    reader = Stripe::Terminal::Reader.process_payment_intent(
+      reader.id,
+      payment_intent: payment_intent.id
+    )
+    resp, status = post_json("/simulate-payment", {
+      reader_id: reader.id
+    })
+    expect(status).to eq(200)
   end
 end
